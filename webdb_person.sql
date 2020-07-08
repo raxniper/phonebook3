4bafd906ac19fb307714c1061595fdc608086332
raxniper <raxniper@naver.com>
2020-07-08 17:56:24 +0900

    1

--- webdb_person.sql
+++ webdb_person.sql
@@ -1,3 +1,4 @@
+-- create person table
 create table person(
     person_id       number(5) not null,
     name            varchar2(30) not null,
@@ -6,25 +7,28 @@ create table person(
     primary key (person_id)
 );
 
-drop table person;
-drop sequence seq_person_id;
-
+-- create seq_person_id sequence
 CREATE SEQUENCE seq_person_id
 INCREMENT BY 1 
 START WITH 1 ;
 
+-- drop sequence, table
+drop sequence seq_person_id;
+drop table person;
 
-INSERT INTO person VALUES (seq_person_id.nextval, '정우성', '010-1111-1111', '02-1111-1111' );
-INSERT INTO person VALUES (seq_person_id.nextval, '이효리', '010-2222-2222', '02-1111-1111' );
-INSERT INTO person VALUES (seq_person_id.nextval, '류승룡', '010-3333-3333', '02-1111-1111' );
-INSERT INTO person VALUES (seq_person_id.nextval, '기안84', '010-4444-4444', '02-1111-1111' );
-INSERT INTO person VALUES (seq_person_id.nextval, '김현수', '010-5555-5555', '02-1111-1111' );
+-- insert data person table
+INSERT INTO person VALUES (seq_person_id.nextval, '이효리', '010-1111-1111', '02-1111-1111' );
+INSERT INTO person VALUES (seq_person_id.nextval, '정우성', '010-2222-2222', '02-2222-2222' );
+INSERT INTO person VALUES (seq_person_id.nextval, '유재석', '010-3333-3333', '02-3333-3333' );
+INSERT INTO person VALUES (seq_person_id.nextval, '이정재', '010-4444-4444', '02-4444-4444' );
+INSERT INTO person VALUES (seq_person_id.nextval, '서장훈', '010-5555-5555', '02-5555-5555' );
 
-
+-- select data person table
 select  person_id,
         name,
         hp,
         company
 from person;
 
+-- commit
 COMMIT;
